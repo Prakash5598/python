c5c39580a26f2f8c5f882f68140c500f83062180
Prakash5598 <51951604+Prakash5598@users.noreply.github.com>
2019-06-20 09:29:24 +0530

    Add files via upload

---
+++ List Comprehension.py
@@ -0,0 +1,7 @@
+'''Jarvis has a list of names and he wants to convert the list items into uppercase. Write a function that takes list and converts all the items in the list to upper
+
+PS: Use list comprehension to solve this'''
+#sol:
+jarvis=['hii','bye','tata']
+x=[y.upper() for y in jarvis]
+print(x)
