f6dcb2647e7eb83f53924aa8ac6d21b287a7d7d1
Prakash5598 <51951604+Prakash5598@users.noreply.github.com>
2019-06-30 01:46:13 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,13 @@
 get_number=int(input())
 temp=get_number
 palin=0
-if get_number<=1000:
-    while get_number!=0:
+while get_number!=0:
          digit=get_number%10
          palin=palin*10+digit
          get_number=get_number//10
-    if temp==palin:
-        print("yes")
-    else:
-        print("no")
+if temp==palin:
+    print("yes")
+else:
+    print("no")
 
     
